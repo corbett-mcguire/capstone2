d861210decfd4332d495fa1cede818a493788bb2
Corbett McGuire <corbett.mcguire@gmail.com>
2022-02-08 20:08:51 -0500

    added type repository

---
+++ src/main/java/com/example/capstone2/repository/TypeRepository.java
@@ -0,0 +1,7 @@
+package com.example.capstone2.repository;
+
+import com.example.capstone2.model.Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeRepository extends JpaRepository <Type, Long> {
+}
