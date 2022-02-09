13ae78881d7243804dc421a3d5e72fa90fc9bb74
Corbett McGuire <corbett.mcguire@gmail.com>
2022-02-08 20:18:45 -0500

    set up controller and service

--- src/main/java/com/example/capstone2/controller/MainController.java
+++ src/main/java/com/example/capstone2/controller/MainController.java
@@ -1,12 +1,23 @@
 package com.example.capstone2.controller;
 
 
+import com.example.capstone2.service.MainService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping(path = "/api")
 public class MainController {
+    private static final Logger LOGGER = Logger.getLogger(MainController.class.getName());
+    private MainService mainService;
+
+    @Autowired
+    public void setMainService(MainService mainService) {
+        this.mainService = mainService;
+    }
 }
--- src/main/java/com/example/capstone2/service/MainService.java
+++ src/main/java/com/example/capstone2/service/MainService.java
@@ -1,7 +1,26 @@
 package com.example.capstone2.service;
 
+import com.example.capstone2.controller.MainController;
+import com.example.capstone2.repository.PostRepository;
+import com.example.capstone2.repository.TypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 public class MainService {
+    private static final Logger LOGGER = Logger.getLogger(MainController.class.getName());
+    private PostRepository postRepository;
+    private TypeRepository typeRepository;
+
+    @Autowired
+    public void setPostRepository(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Autowired
+    public void setTypeRepository(TypeRepository typeRepository) {
+        this.typeRepository = typeRepository;
+    }
 }
